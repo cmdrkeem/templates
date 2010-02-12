4e8fa2c903d9fe56d8f3224b51344091d19e2ace
Chris Meek <cmdrkeem@gmail.com>
2010-02-12 22:05:52 +0000

    initial commit

---
+++ fubumvc/src/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="__NAME__.Global" Language="C#" %>
---
+++ fubumvc/src/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using FubuMVC.StructureMap.Bootstrap;
+
+namespace __NAME__
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var routeCollection = RouteTable.Routes;
+            FubuStructureMapBootstrapper.Bootstrap(routeCollection);
+        }
+    }
+}
